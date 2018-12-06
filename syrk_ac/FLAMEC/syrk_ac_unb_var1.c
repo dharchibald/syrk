461b64209b0eca943d8e55aa90e9141f8300532e
ctjoe <ctjoe@cs.utexas.edu>
2018-12-06 15:58:04 -0600

    Added C code for syrk

--- syrk_ac/FLAMEC/syrk_ac_unb_var1.c
+++ syrk_ac/FLAMEC/syrk_ac_unb_var1.c
@@ -1,51 +1,55 @@
+
+/* Copyright 2018 The University of Texas at Austin  
+ 
+   For licensing information see
+                  http://www.cs.utexas.edu/users/flame/license.html 
+
+   Programmed by: Name of author
+                  Email of author
+                                                                     */
+
 #include "FLAME.h"
 
-int syrk_ac_unb_var1( FLA_Obj A, FLA_Obj C )
+int Syrk_ut(A,C)_unb_var1( FLA_Obj A, FLA_Obj C )
 {
-  FLA_Obj LTL,   LTR,      L00,  l01,      L02, 
-          LBL,   LBR,      l10t, lambda11, l12t,
-                           L20,  l21,      L22;
+  FLA_Obj AL,    AR,       A0,  a1,  A2;
 
-  FLA_Obj BT,              B0,
-          BB,              b1t,
-                           B2;
+  FLA_Obj CTL,   CTR,      C00,  c01,     C02, 
+          CBL,   CBR,      c10t, gamma11, c12t,
+                           C20,  c21,     C22;
 
-  FLA_Part_2x2( L,    &LTL, &LTR,
-                      &LBL, &LBR,     0, 0, FLA_BR );
+  FLA_Part_1x2( A,    &AL,  &AR,      0, FLA_LEFT );
 
-  FLA_Part_2x1( B,    &BT, 
-                      &BB,            0, FLA_BOTTOM );
+  FLA_Part_2x2( C,    &CTL, &CTR,
+                      &CBL, &CBR,     0, 0, FLA_TL );
 
-  while ( FLA_Obj_length( LBR ) < FLA_Obj_length( L ) ){
+  while ( FLA_Obj_width( AL ) < FLA_Obj_width( A ) ){
 
-    FLA_Repart_2x2_to_3x3( LTL, /**/ LTR,       &L00,  &l01,      /**/ &L02,
-                                                &l10t, &lambda11, /**/ &l12t,
-                        /* ************* */   /* *************************** */
-                           LBL, /**/ LBR,       &L20,  &l21,      /**/ &L22,
-                           1, 1, FLA_TL );
+    FLA_Repart_1x2_to_1x3( AL,  /**/ AR,        &A0, /**/ &a1, &A2,
+                           1, FLA_RIGHT );
 
-    FLA_Repart_2x1_to_3x1( BT,                &B0, 
-                                              &b1t, 
-                        /* ** */            /* *** */
-                           BB,                &B2,        1, FLA_TOP );
+    FLA_Repart_2x2_to_3x3( CTL, /**/ CTR,       &C00,  /**/ &c01,     &C02,
+                        /* ************* */   /* ************************** */
+                                                &c10t, /**/ &gamma11, &c12t,
+                           CBL, /**/ CBR,       &C20,  /**/ &c21,     &C22,
+                           1, 1, FLA_BR );
 
     /*------------------------------------------------------------*/
 
-    FLA_Ger( FLA_ONE, l21, b1t, B2 );
-    FLA_Scal( lambda11, b1t );
+    /*                       update line 1                        */
+    /*                             :                              */
+    /*                       update line n                        */
 
     /*------------------------------------------------------------*/
 
-    FLA_Cont_with_3x3_to_2x2( &LTL, /**/ &LTR,       L00,  /**/ l01,      L02,
-                            /* ************** */  /* ************************* */
-                                                     l10t, /**/ lambda11, l12t,
-                              &LBL, /**/ &LBR,       L20,  /**/ l21,      L22,
-                              FLA_BR );
+    FLA_Cont_with_1x3_to_1x2( &AL,  /**/ &AR,        A0, a1, /**/ A2,
+                              FLA_LEFT );
 
-    FLA_Cont_with_3x1_to_2x1( &BT,                B0, 
-                            /* ** */           /* *** */
-                                                  b1t, 
-                              &BB,                B2,     FLA_BOTTOM );
+    FLA_Cont_with_3x3_to_2x2( &CTL, /**/ &CTR,       C00,  c01,     /**/ C02,
+                                                     c10t, gamma11, /**/ c12t,
+                            /* ************** */  /* ************************ */
+                              &CBL, /**/ &CBR,       C20,  c21,     /**/ C22,
+                              FLA_TL );
 
   }
 
